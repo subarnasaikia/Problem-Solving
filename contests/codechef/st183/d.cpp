f7f74565fb23cc6d36ba19fc273b31c1ecedd0b2
Subarna Saikia <saikiasubarna74@gmail.com>
2025-04-24 13:31:13 +0530

    solutions for startes 183 codechef. - solved during the contest.

---
+++ contests/codechef/st183/d.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using  namespace std;
+// #include <ext/pb_ds/assoc_container.hpp>
+// using namespace __gnu_pbds;
+
+void fastio(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
+#define ll long long
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair< string, string> pss;
+typedef pair<string, int> psi;
+typedef vector<pii> vpi;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef vector<pll> vpll;
+typedef stack<int> sti;
+typedef stack<char> stc;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+
+#define int long long int
+#define input() freopen("input.txt", "r", stdin);
+#define output() freopen("output.txt", "w", stdout);
+#define error() freopen("error.txt", "w", stderr);
+#define REP(a, b, c) for(int(a) = (b); (a) < (c); (a)++ )
+#define rep(a, c) REP(a, 0, c)
+#define repe(a,b,c) for(int(a) = (b); (a) <= (c); (a)++)
+#define repl(a,b,c) for(ll(a) = (b); (a) < (c); (a)++)
+#define repd(a, b, c) for(int(a) = (b); (a) >= (c); --(a));
+#define RESET(a,b) memset(a, b, sizeof(a))
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define all(v) (v).begin() , (v).end()
+#define lb(a) lower_bound(all(v), a)
+#define ub(a) upper_bound(all(v),a)
+#define maxv(v) *max_element(all(v))
+#define minv(v) *min_element(all(v))
+#define allg(v) all(v), greater<int>()
+#define permute next_permutation
+#define sb(n) __builtin_popcount(n)
+#define YES cout<<"YES\n";
+#define NO cout<<"NO\n";
+#define endl '\n'
+#define line cout<<endl;
+// const int MAX = 9223372036854775807;
+
+static int subarna_sqrt(int val){
+    int left = 1, right = val;
+    while(left < right)
+    {
+        int mid = left + (right - left)/2;
+        if(mid >= val/mid) right = mid;
+        else left = mid + 1;
+    }
+    return left;
+} 
+
+int nxt(){
+    int n;
+    cin >> n;
+    return n;
+}
+
+
+void solve(){
+    // Main solution goes here
+    int n=nxt(), m=nxt();
+    int edges = n*(n - 1ll)/2ll;
+    int zeros = edges - m;
+    int smalles_val = (1ll + (subarna_sqrt(1ll*zeros*8 + 1ll)))/2;
+    // cout << smalles_val << endl;
+    if(smalles_val * (smalles_val - 1ll)/2 < zeros) smalles_val++;
+    int mini = (zeros < (n-1ll)) ? (n - 1ll - zeros) : 0ll; 
+    int maxi = n - smalles_val;
+    // cout << mini << " " << maxi << endl;
+    cout << ((mini > maxi) ? 0ll : (maxi - mini + 1ll)*(mini + maxi)/2) << endl;
+}
+
+signed main(){
+    fastio();
+
+    #ifdef ONLINE_JUDGE
+    //input(); output(); error();
+    #endif
+
+    int tc=1;
+    cin>>tc;
+    while (tc--) solve();
+
+    cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<"secs"<<endl;
+    return 0;
+
+}
